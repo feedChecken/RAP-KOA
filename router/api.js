c1487e76e64b0dbec5eb364a96c0a5002b660340
hawk <whktk@qq.com>
2015-09-14 13:34:51 +0800

    add,js hintrc

--- router/api.js
+++ router/api.js
@@ -1,5 +1,5 @@
 var getData = {
-  getUserProject: function*(app) {
+  getUserProject: function(app) {
     // this.body = this.models
     return new Promise(function(resolve, reject){
       app.models.Project.findAll({
@@ -10,36 +10,20 @@ var getData = {
         var ret = [];
         res.forEach(function(val){
           ret.push(val.intro);
-        })
+        });
         resolve(ret);
-      })
+      });
     });
   },
-  getUserJoinProject : function*(app){
-    return new Promise(function(resolve, reject) {
-      app.models.User.findOne({
-        include : [app.models.Project],
-        where : {
-          id : 1
-        }
-      }).then(function(res){
-        var ret = [];
-        res.Projects.forEach(function(val){
-          ret.push(val.intro);
-        })
-        resolve(ret);
-      })
-    })
-  },
-  testQuery : function*(app){
+  getUserJoinProject : function(app){
     return new Promise(function(resolve, reject){
-       app.orm.query('SELECT * FROM tb_project LEFT JOIN (SELECT name FROM tb_user) ON tb_project.user_id=tb_user.id').spread(function(res,metadata){
+       app.orm.query('SELECT a.name as owner,b.id,b.name,b.update_time as \'update\',b.introduction as intro FROM tb_user as a INNER JOIN tb_project as b where a.id =1').spread(function(res,metadata){
          resolve(res);
-       })
+       });
     });
   }
-}
+};
 module.exports = function*(app, apiname) {
   var data = yield getData[apiname](app);
   return data;
-}
+};
